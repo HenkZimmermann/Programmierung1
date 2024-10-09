a6689387b70a31ca917c697bc965566ca6bb2ce8
HenkZimmermann <henk.z@gmx.de>
2024-10-09 12:04:02 +0200

    first commit

---
+++ Aufgabe5.java
@@ -0,0 +1,21 @@
+public class Aufgabe5 {
+
+    public static void main(String[]args){
+        warning();;
+
+    }
+    
+    static int product(int x, int y){
+        return(int)x*y;
+    }
+    static int squaresum(int x, int y){
+        return(x*x)+(y*y);
+    }
+    static void output(String content){
+        System.out.println(content);
+    }
+    static void warning(){
+        System.out.println("WARNUNG!");
+    }
+}
+
