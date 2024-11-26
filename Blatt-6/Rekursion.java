cc645f29c6e4c60a22fd34fd95aa810ddf3858ae
HenkZimmermann <henk.z@gmx.de>
2024-11-26 15:57:36 +0100

    Übung 6

--- Blatt-6/Rekursion.java
+++ Blatt-6/Rekursion.java
@@ -13,24 +13,29 @@ public class Rekursion {
     }
 
     static String toString1(final SignlyLinkedList list){
-        if(list.root == null){
-            return " ";
+        if(list.root.next() == null){
+            return " "+Integer.toString(list.root.value());
         }
-        String retString = list.root().toString();
+
+        String retString = " "+Integer.toString(list.root.value());
         retString = retString + toString1(new SignlyLinkedList(list.root.next()));
         return retString;
     }
+    /*
     static SignlyLinkedList addLast(final SignlyLinkedList list, final int value){
         if(list.root == null){
             return new SignlyLinkedList(value, null)
         }
 
     }
+         */
 
     public static void main(String[]args){
-        SignlyLinkedList sll = new SignlyLinkedList(new ListNode(10,null));
-        addFirst(sll, 15);
-        toString1(sll);
+        ListNode kn1 = new ListNode(10,null);
+        ListNode kn2 = new ListNode(15,kn1);
+        SignlyLinkedList sll = new SignlyLinkedList(kn2);
+
+        System.out.print(toString1(sll));
         
 
     }
