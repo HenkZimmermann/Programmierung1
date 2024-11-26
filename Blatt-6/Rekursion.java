fe4acfa95c8eec5d7bc8296496eb03bbc892c3cc
HenkZimmermann <henk.z@gmx.de>
2024-11-26 16:25:43 +0100

    funktioniert

--- Blatt-6/Rekursion.java
+++ Blatt-6/Rekursion.java
@@ -14,26 +14,49 @@ public class Rekursion {
 
     static String toString1(final SignlyLinkedList list){
         if(list.root.next() == null){
-            return " "+Integer.toString(list.root.value());
+            return " "+Integer.toString(list.root.value()) + " ";
         }
 
         String retString = " "+Integer.toString(list.root.value());
         retString = retString + toString1(new SignlyLinkedList(list.root.next()));
         return retString;
     }
-    /*
+    
     static SignlyLinkedList addLast(final SignlyLinkedList list, final int value){
-        if(list.root == null){
-            return new SignlyLinkedList(value, null)
+        if(list.root.next() == null){
+            ListNode lastNode = new ListNode(value,null);
+            ListNode beforeLastNode = new ListNode(list.root.value(),lastNode);
+            return new SignlyLinkedList(beforeLastNode);
         }
+        ListNode node = new ListNode (list.root.value(), addLast(new SignlyLinkedList(list.root.next()), value).root());
+        return new SignlyLinkedList(node);
 
     }
-         */
+
+
+    static SignlyLinkedList remove(final SignlyLinkedList list, final int value){
+        if(list.root.value() == value){
+            return new SignlyLinkedList(list.root.next());
+        }
+        return remove(new SignlyLinkedList(list.root.next()),value);
+
+    }
+
+
+
+        
+
+
+         
 
     public static void main(String[]args){
         ListNode kn1 = new ListNode(10,null);
         ListNode kn2 = new ListNode(15,kn1);
         SignlyLinkedList sll = new SignlyLinkedList(kn2);
+        sll = addLast(sll, 19);
+        sll = addLast(sll, 17);
+        sll = remove(sll, 17);
+
 
         System.out.print(toString1(sll));
         
